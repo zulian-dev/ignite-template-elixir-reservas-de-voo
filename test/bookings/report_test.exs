0f1e246009251e14af061e681475d587814f603b
Zulian <luiz.zulian@paketa.com.br>
2021-09-20 23:54:38 -0300

    testes geracao de relatorio por intervalo de datas

--- test/bookings/report_test.exs
+++ test/bookings/report_test.exs
@@ -4,6 +4,7 @@ defmodule Flightex.Bookings.ReportTest do
   use ExUnit.Case, async: true
 
   alias Flightex.Bookings.Report
+  import Flightex.Factory
 
   @filename "report-test.csv"
 
@@ -31,6 +32,61 @@ defmodule Flightex.Bookings.ReportTest do
       File.rm(@filename)
 
       assert file =~ content
+    end
+  end
+
+  describe "generate_report/2" do
+    setup do
+      Flightex.start_agents()
+
+      :ok
+    end
+
+    test "when twoo dates has given, return the report with flighs btween dates" do
+      :booking
+      |> build(
+        complete_date: ~N[2001-10-08 15:30:00],
+        local_origin: "La Paz, Bolivia",
+        local_destination: "Charlotte, United States"
+      )
+      |> Flightex.create_or_update_booking()
+
+      :booking
+      |> build(
+        complete_date: ~N[2003-12-01 12:20:00],
+        local_origin: "San Francisco, United States",
+        local_destination: "Ashgabat, Turkmenistan"
+      )
+      |> Flightex.create_or_update_booking()
+
+      :booking
+      |> build(
+        complete_date: ~N[2004-04-12 05:24:00],
+        local_origin: "Barcelona, Spain",
+        local_destination: "Ottawa, Canada"
+      )
+      |> Flightex.create_or_update_booking()
+
+      :booking
+      |> build(
+        complete_date: ~N[2015-04-29 10:24:00],
+        local_origin: "Salt Lake City, United States",
+        local_destination: "Boston, United States"
+      )
+      |> Flightex.create_or_update_booking()
+
+      Report.generate_report(@filename, ~N[2001-01-01 00:00:00], ~N[2004-01-01 00:00:00])
+
+      {:ok, file} = File.read(@filename)
+      File.rm(@filename)
+
+      response = file
+
+      expected_response =
+        "12345678900,La Paz, Bolivia,Charlotte, United States,2001-10-08 15:30:00" <>
+          "12345678900,San Francisco, United States,Ashgabat, Turkmenistan,2003-12-01 12:20:00"
+
+      assert response == expected_response
     end
   end
 end
